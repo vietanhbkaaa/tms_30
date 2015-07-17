e13b0784513385627de9f8c772fba53b0c100a15
nhs3108 <nhs3108@gmail.com>
2015-07-17 09:25:19 +0700

    Using gem DEVISE to build login/logout features

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  devise :database_authenticatable, :rememberable, :trackable, :validatable
   has_many :user_courses, dependent: :destroy
   has_many :user_subjects, dependent: :destroy
   has_many :user_tasks, dependent: :destroy

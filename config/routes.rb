46b83f9fc671af68ed7d1b7894816b7bc56b88be
nhs3108 <nhs3108@gmail.com>
2015-07-17 13:15:04 +0700

    Design dashboard page - first step

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    root "dashboards#index"
+  end
+
   devise_for :users, path: "", path_names: {
     sign_in: "login",
     sign_out: "logout"

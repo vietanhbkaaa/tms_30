248b5c8918f363ce52334d7d0a5e25edb44df5ad
nhs3108 <nhs3108@gmail.com>
2015-07-17 14:07:51 +0700

    Update relationship betweens models

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,6 @@
 class Course < ActiveRecord::Base
   has_many :user_courses, dependent: :destroy
   has_many :course_subjects, dependent: :destroy
+  has_many :users, through: :user_courses
+  has_many :subjects, through: :course_subjects
 end

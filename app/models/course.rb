35c88d965d0a218634a13ce4d5596f5991d0987a
nhs3108 <nhs3108@gmail.com>
2015-07-18 11:04:54 +0700

    Update application logic

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,13 +4,15 @@ class Course < ActiveRecord::Base
   has_many :user_courses, dependent: :destroy
   has_many :supervisor_courses, dependent: :destroy
   has_many :course_subjects, dependent: :destroy
-  has_many :users, through: :user_courses
-  has_many :subjects, through: :course_subjects
+  has_many :users, through: :user_courses, dependent: :destroy
+  has_many :subjects, through: :course_subjects, dependent: :destroy
   has_many :supervisors, through: :supervisor_courses
+  has_many :tasks, through: :subjects
   scope :recent, ->{order created_at: :desc}
 
   validates :name, presence: Settings.courses.is_present,
     length: {minimum: Settings.courses.min_c, maximum: Settings.courses.max_c}
+  before_create {self.closed = true}
 
   def date_duration
     self.subjects.inject(0){|total_date, subject| total_date + subject.date_duration}
--- app/models/course_subject.rb
+++ app/models/course_subject.rb
@@ -1,4 +1,21 @@
 class CourseSubject < ActiveRecord::Base
   belongs_to :course
   belongs_to :subject
+
+  before_create {self.closed = true}
+  after_create :init_user_subjects
+  after_destroy :remove_user_subjects
+
+  private
+  def init_user_subjects
+    self.course.users.each do |user|
+      UserSubject.create user_id: user.id, subject_id: self.subject_id
+    end
+  end
+
+  def remove_user_subjects
+    self.course.users.each do |user|
+      UserSubject.find_by(user_id: user.id, subject_id: self.subject_id).destroy
+    end
+  end
 end

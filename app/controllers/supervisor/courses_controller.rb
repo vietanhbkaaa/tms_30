678085a357f76664c37f95a189d1947e115435a7
vietanhbkaaa <pham.viet.anh@framgia.com>
2015-08-07 09:48:42 +0700

    update activity

--- app/controllers/supervisor/courses_controller.rb
+++ app/controllers/supervisor/courses_controller.rb
@@ -1,7 +1,7 @@
 class Supervisor::CoursesController < ApplicationController
   before_action :init_course, except: [:index, :new, :create]
   load_and_authorize_resource :course
-  
+
   def index
     @courses = current_user.managing_courses.paginate page: params[:page],
       per_page: Settings.courses.per_page
@@ -30,7 +30,7 @@ class Supervisor::CoursesController < ApplicationController
     end
     redirect_to supervisor_courses_path
   end
-  
+
   private
   def init_course
     @course = Course.find params[:id]
--- app/models/user_task.rb
+++ app/models/user_task.rb
@@ -1,7 +1,14 @@
 class UserTask < ActiveRecord::Base
+  include Logs
   belongs_to :user_subject
   belongs_to :task
 
   scope :finished, ->{where finished: true}
 
+  after_update :finished_task_activity
+
+  private
+  def finished_task_activity
+    create_activity Settings.logs.finished, user_subject.user.id, self.id, task.name
+  end
 end
